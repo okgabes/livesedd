a853745682e5ff7c7a6ddfadca34379a58dd1e5c
Gabriel Teixeira <gabriel.teixeira009@gmail.com>
2022-08-29 21:24:10 -0400

    descricao da atividade

---
+++ lives.py
@@ -0,0 +1,8 @@
+""""
+Implemente um TAD – Tipo Abstrato de Dados (VETOR DE REGISTROS ou LISTA ENCADEADA SIMPLES) denominado LIVE, com os seguintes campos: título, data, hora, plataforma (Youtube, Instagram, etc), apresentador, artista. Para a estrutura criada, implemente as seguintes operações:
+a) Cadastrar uma nova LIVE, com todos os campos;
+b) Pesquisar uma determinada LIVE pelo campo artista**;
+c) Exibir todos os dados das LIVES cadastradas;
+d) fazer um programa (em C/++ ou Python) para testar o seu TAD
+
+"""
